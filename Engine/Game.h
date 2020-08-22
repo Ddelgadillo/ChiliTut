69fc1c53cbe369180592d3f9c9c918102f2dfab6
Diego Delgadillo <ddelgadillo11@gmail.com>
2020-08-22 09:43:12 -0500

    Added reticle function

--- Engine/Game.h
+++ Engine/Game.h
@@ -37,6 +37,7 @@ private:
 	/********************************/
 	/*  User Functions              */
 	/********************************/
+    void Reticle();
 private:
 	MainWindow& wnd;
 	Graphics gfx;
